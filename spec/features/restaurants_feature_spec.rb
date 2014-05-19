46c669433fe16f69f8156d78dab6a6bca7dbd3a6
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-19 16:45:57 +0100

    model generated

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,3 +8,14 @@ describe 'restaurants index page' do
     end
   end
 end
+
+describe 'creating a restaurant' do
+  it 'adds it to the restaurants index' do
+    visit 'restaurants/new'
+    fill_in 'Name',     with: 'Moro'
+    fill_in 'Address',  with: '34 Exmouth Market, London'
+    click_button 'Create Restaurant'
+    expect(current_path).to eq   '/restaurants'
+    expect(page).to have_content 'Moro'
+  end
+end
