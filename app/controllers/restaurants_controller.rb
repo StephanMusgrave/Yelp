a3ea637e5cacea42ce9414287826f9a30ec8a99d
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-19 17:21:26 +0100

    2nd test - creates a resataurant passes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,16 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @restaurants =Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create({name: 'Moro', address: '34 Exmouth Market'})
+    redirect_to '/restaurants'
   end
 
 end
