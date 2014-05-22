8c2c8fb6e0fd95e8d7cd78d42be9e4f0bf8f7962
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-22 16:15:06 +0100

    getting started with Javascript

---
+++ app/assets/javascripts/restaurants.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.new_review').on('submit', function(event){
+  event.preventDefault();
+  var reviewList = $(this).siblings('ul');
+  var currentRestaurant = $(this).parent();
+  console.log(currentRestaurant);
+
+  $.post($(this).attr('action'), $(this).serialize(), function(review){
+    var newReview = Mustache.render($('#review_template').html(), review);
+    reviewList.append(newReview);
+
+    currentRestaurant.find('.review_count').text(review.restaurant.review_count);
+    currentRestaurant.find('.average_rating_number').text(review.restaurant.review_count);
+    currentRestaurant.find('.average_rating_stars').text(review.restaurant.average_rating_stars);
+    });
+  });
+});
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,15 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
+  # def new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = Review.new
+  # end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create!(params[:review].permit(:thoughts, :rating))
+    @review = @restaurant.reviews.create!(params[:review].permit(:thoughts, :rating))
 
-    redirect_to '/restaurants'
+    # redirect_to '/restaurants'
+  render json: @review
+
   end
 end
