7cf6bcf36e928cfca33ad6e24f6f48cdc6e26c4b
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-23 15:11:40 +0100

    user login partially implemented

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -10,63 +10,91 @@ describe 'restaurants index page' do
 end
 
 describe 'creating a restaurant' do
-  context 'with valid data' do
-    it 'adds it to the restaurants index' do
-      visit 'restaurants/new'
-      fill_in 'Name', with: 'Moro'
-      fill_in 'Address', with: '34 Exmouth Market, London'
-      fill_in 'Cuisine', with: 'Spanish'
-      click_button 'Create Restaurant'
 
-      expect(current_path).to eq   '/restaurants'
-      expect(page).to have_content 'Moro'
+  context 'logged out' do
+    it 'takes us to sign up page' do
+      visit '/restaurants'
+      click_link 'Create a restaurant'
+      expect(page).to have_content 'Sign up'
     end
   end
+  # context 'logged in' do
+  #   context 'with valid data' do
+  #     it 'adds it to the restaurants index' do
+  #       visit 'restaurants/new'
+  #       fill_in 'Name', with: 'Moro'
+  #       fill_in 'Address', with: '34 Exmouth Market, London'
+  #       fill_in 'Cuisine', with: 'Spanish'
+  #       click_button 'Create Restaurant'
 
-  context 'with invalid data' do
-    it 'shows an error' do
-      visit 'restaurants/new'
-      fill_in 'Name', with: 'moro'
-      fill_in 'Address', with: '34'
-      click_button 'Create Restaurant'
-      expect(page).to have_content 'error'
-    end
-  end
+  #       expect(current_path).to eq   '/restaurants'
+  #       expect(page).to have_content 'Moro'
+  #     end
+  #   end
 
+  #   context 'with invalid data' do
+  #     it 'shows an error' do
+  #       visit 'restaurants/new'
+  #       fill_in 'Name', with: 'moro'
+  #       fill_in 'Address', with: '34'
+  #       click_button 'Create Restaurant'
+  #       expect(page).to have_content 'error'
+  #     end
+  #   end
+  # end
 end
 
 describe 'editing a restaurant' do
   before { Restaurant.create(name: 'Angels', address: 'Camberwell Church Street', cuisine: 'Spanish')}
   
-  context 'with valid data' do
-    it 'saves the change to the restaurant' do
-      visit '/restaurants'
-      click_link 'Edit Angels'
-      fill_in 'Name', with: 'Angels and Gipsies'
-      click_button 'Update Restaurant'
-      expect(current_path).to eq '/restaurants'
-      expect(page).to have_content 'Angels and Gipsies'
+  context 'logged out' do
+    it 'shows no edit link' do
+      visit '/restaurants' 
+      expect(page).not_to have_link 'Edit Angels'
     end
   end
 
-  context 'with invalid data' do
-    it 'displays an error' do
-      visit '/restaurants'
-      click_link 'Edit Angels'
-      fill_in 'Name', with: 'angels and gipsies'
-      click_button 'Update Restaurant'
-      expect(page).to have_content 'error'
-    end
   end
-end
+  # context 'logged in' do
+  #   context 'with valid data' do
+  #     it 'saves the change to the restaurant' do
+  #       visit '/restaurants'
+  #       click_link 'Edit Angels'
+  #       fill_in 'Name', with: 'Angels and Gipsies'
+  #       click_button 'Update Restaurant'
+  #       expect(current_path).to eq '/restaurants'
+  #       expect(page).to have_content 'Angels and Gipsies'
+  #     end
+  #   end
+
+  #   context 'with invalid data' do
+  #     it 'displays an error' do
+  #       visit '/restaurants'
+  #       click_link 'Edit Angels'
+  #       fill_in 'Name', with: 'angels and gipsies'
+  #       click_button 'Update Restaurant'
+  #       expect(page).to have_content 'error'
+  #     end
+  #   end
+  # end
 
 describe 'deleting a restaurant' do
   before { Restaurant.create(name: 'Angels', address: 'Camberwell Church Street', cuisine: 'Spanish')}
-  
-  it 'removes the restaurant from the listing' do
-    visit '/restaurants'
-    click_link 'Delete Angels'
-    expect(page).not_to have_content 'Angels'
-    expect(page).to have_content 'Deleted successfully'
+
+  context 'logged out' do
+    it 'shows no delete link' do
+      visit '/restaurants' 
+      expect(page).not_to have_link 'Delete Angels'
+    end
   end
+
+  # context 'logged in' do
+      
+  #   it 'removes the restaurant from the listing' do
+  #     visit '/restaurants'
+  #     click_link 'Delete Angels'
+  #     expect(page).not_to have_content 'Angels'
+  #     expect(page).to have_content 'Deleted successfully'
+  #   end
+  # end
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,34 +1,43 @@
-require 'spec_helper'
+ require 'spec_helper'
 
 describe 'writing reviews' do
   before { Restaurant.create(name: 'Moro', address: 'Exmouth Market', cuisine: 'Spanish')}
 
-  specify 'restaurants begin with no reviews' do
+  context 'logged out' do 
+    it 'does not show the review form'do
     visit '/restaurants'
-    expect(page).to have_content '0 reviews'
+      expect(page).not_to have_field('Thoughts')
+    end
   end
 
-  it 'adds the review to the restaurant', js: true do
-    leave_review(4, 'Good but noisy')
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content 'Good but noisy'
-    expect(page).to have_content '1 review'
-  end
+  context 'logged in' do 
+    
+    specify 'restaurants begin with no reviews' do
+      visit '/restaurants'
+      expect(page).to have_content '0 reviews'
+    end
 
-  it 'calculates the average of reviews', js: true do
-    leave_review(5, 'Good but noisy')
-    leave_review(2, 'Poor')
-    expect(page).to have_content '3.5'
+    it 'adds the review to the restaurant', js: true do
+      leave_review(4, 'Good but noisy')
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'Good but noisy'
+      expect(page).to have_content '1 review'
+    end
 
+    it 'calculates the average of reviews', js: true do
+      leave_review(5, 'Good but noisy')
+      leave_review(2, 'Poor')
+      expect(page).to have_content '3.5'
+    end
   end
 
   def leave_review(rating, thoughts)
-      visit '/restaurants'
-      # click_link 'Review Moro'
+    visit '/restaurants'
+    # click_link 'Review Moro'
 
-      fill_in 'Thoughts', with: thoughts
-      select rating.to_s, from: 'Rating' 
-      click_button 'Leave Review'
+    fill_in 'Thoughts', with: thoughts
+    select rating.to_s, from: 'Rating' 
+    click_button 'Leave Review'
   end
 
 end
